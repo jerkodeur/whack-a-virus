8c4fbd02f4501e6f95b80af0004834bafe1956c6
hipro90 <hipro90@gmail.com>
2020-04-29 21:54:34 +0200

    create welcome page

---
+++ src/components/Accueil.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Accueil.css'
+import {Link} from 'react-router-dom'
+import Call from './Call-api'
+
+class Accueil extends React.Component {
+
+    render() {
+
+        const handleChange = (e) => {
+            const value = e.target.value
+            sessionStorage.setItem('name',value)
+        }
+    return (
+        <div className="name">               
+                <label htmlfor="name">What your name?</label>
+                <input type="text" id="name" name="name" required onChange={handleChange}/>  
+                <Link to="/"><button>Go!</button></Link> 
+                <Call/>                  
+        </div>
+        )
+    }
+}
+
+export default Accueil
