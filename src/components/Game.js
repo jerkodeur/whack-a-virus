e7eaba491bae6383ed7840c9d69b852ce07086bb
jerkodeur <jerome.potie@gmail.com>
2020-04-30 08:37:45 +0200

    The game is functionnal, add images in place of the circles
    Co-authored-by: julienrousseau16 <julienrousseau16@gmail.com>
    Co-authored-by: orko3012 <stephane.bour.1979@gmail.com>
    Co-authored-by: Paracelse-ITOUA <paraitoua@gmail.com>

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import LifeBar from './LifeBar'
 import Call from './Call'
+import corona from './images/coronavirus.png'
+import anticorps from './images/anticorps.png'
 
 
 // const maxX = rectWidth - (circleWidth)
@@ -19,11 +21,33 @@ class Game extends React.Component {
     state = {
         circle: '',
         circle2: '',
-        rectangle:'',
+        rectangle: '',
         rectWidth: 0,
         rectHeight: 0,
         circleWidth: 0,
-        circleHeight: 0
+        circleHeight: 0,
+        health: 37,
+        win: '',
+        lifeBarColor: "",
+    }
+
+    //  change lifeBarColor
+    changeColor = () => {
+        this.state.health > 75
+            ? this.setState({ lifeBarColor: "red" })
+            : this.state.health <= 75 && this.state.health > 50
+                ? this.setState({ lifeBarColor: "orange" })
+                : this.state.health <= 50 && this.state.health > 25
+                    ? this.setState({ lifeBarColor: "yellow" })
+                    : this.setState({ lifeBarColor: "green" });
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.health !== prevState.health) {
+            this.changeColor();
+            this.state.health === 100 ? this.setState({ win: 'perdu' }) : this.state.health === 7 && this.setState({ win: 'gagné' })
+            // if (this.state.health === 100 || this.state.health === 7) this.setState({ health: 37 })
+        }
     }
 
     definePosition = () => {
@@ -35,13 +59,11 @@ class Game extends React.Component {
         const posY2 = Math.random() * maxY
         const circle1 = this.state.circle
         const circle2 = this.state.circle2
-        if (circle1) { circle1.style.left = `${posX}px`  }
+        if (circle1) { circle1.style.left = `${posX}px` }
         if (circle1) { circle1.style.top = `${posY}px` }
         if (circle2) { circle2.style.left = `${posX2}px` }
         if (circle2) { circle2.style.top = `${posY2}px` }
     }
-
-
 
     componentDidMount() {
         const rectangle = document.getElementById("rectangle")
@@ -52,8 +74,35 @@ class Game extends React.Component {
         const circleWidth = parseFloat(getComputedStyle(circle).width)
         const circleHeight = parseFloat(getComputedStyle(circle).height)
         this.setState({ circle: circle, circle2: circle2, rectangle: rectangle, rectWidth: rectWidth, rectHeight: rectHeight, circleWidth: circleWidth, circleHeight: circleHeight })
-        setInterval(this.definePosition, 1000)
+        setInterval(this.definePosition, 900)
+
+        setInterval(this.permanentDecrease, 1000)
     }
+
+    handleClick = (e) => {
+        const id = e.target.id;
+        // console.log(this.state.health, "health");
+        id === "circle"
+            ? this.increaseHealth()
+            : this.decreaseHealth();
+    };
+    permanentDecrease = () => {
+        this.state.health < 100 && this.setState({ health: this.state.health + 1 })
+        this.state.health === 100 && this.setState({ win: 'perdu' })
+    }
+    // Increase the health permanently until 100
+    increaseHealth = () => {
+        this.state.health <= 80 && this.setState({ health: this.state.health + 10 })
+        // this.state.health === 100 && this.setState({ win: 'perdu' })
+    };
+    // Decrease the health if click on the monster
+    decreaseHealth = () => {
+        this.state.health >= 27
+            ? this.setState({ health: this.state.health - 10 })
+            : this.setState({ health: 7 });
+        // this.state.health === 7 && this.setState({ win: 'gagné' })
+        console.log(this.state.health)
+    };
 
     render() {
 
@@ -65,14 +114,18 @@ class Game extends React.Component {
                 <div className="empty"></div>
                 <div className="flex">
                     <div className="areaGame">
-                        <div className="rectangle" id="rectangle" style={{backgroundImage:`url(${image})`}}>
-                            <div className="circle" id="circle"></div>
-                            <div className="circle2" id="circle2"></div>
+                        <div className="rectangle" id="rectangle" style={{ backgroundImage: `url(${image})` }}>
+                            {/* <div className="circle" id="circle" style={{ backgroundImage: `url(${corona})` }} onClick={this.handleClick}><h2>up</h2></div> */}
+                            <img className="circle" id="circle" src={corona} onClick={this.handleClick} alt="" />
+                            {/* <div className="circle2" id="circle2" onClick={this.handleClick}><h3>down: {this.state.health}</h3></div> */}
+                            <img className="circle2" id="circle2" src={anticorps} onClick={this.handleClick} alt="" />
                         </div>
                     </div>
-                    <LifeBar />
+                    <LifeBar health={this.state.health} lifeBarColor={this.state.lifeBarColor} />
                 </div>
                 <div>{this.definePosition()}</div>
+                <h1>{this.state.health}</h1>
+                <p>{this.state.win === 'perdu' ? "Perdu!!!" : this.state.win === "gagné" && "Gagné!!!"}</p>
             </div>
         )
     }
--- src/components/LifeBar.js
+++ src/components/LifeBar.js
@@ -3,46 +3,7 @@ import React from "react";
 import "./LifeBar.css";
 
 class LifeBar extends React.Component {
-  state = {
-    damage: 10,
-    health: 7,
-    lifeBarColor: "",
-  };
 
-  // change lifeBarColor
-  changeColor = () => {
-    this.state.health > 75
-      ? this.setState({ lifeBarColor: "red" })
-      : this.state.health <= 75 && this.state.health > 50
-      ? this.setState({ lifeBarColor: "orange" })
-      : this.state.health <= 50 && this.state.health > 25
-      ? this.setState({ lifeBarColor: "yellow" })
-      : this.setState({ lifeBarColor: "green" });
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.health !== prevState.health) {
-      this.changeColor();
-    }
-  }
-  // Change the health depending the context
-  getHealth = (e) => {
-    const button = e.target.id;
-    console.log(this.state.health, "health");
-    button === "up"
-      ? setInterval(this.increaseHealth, 100)
-      : this.decreaseHealth();
-  };
-  // Increase the health permanently until 100
-  increaseHealth = () => {
-    this.state.health < 100 && this.setState({ health: this.state.health + 1 });
-  };
-  // Decrease the health if click on the monster
-  decreaseHealth = () => {
-    this.state.health >= 27
-      ? this.setState({ health: this.state.health - 20 })
-      : this.setState({ health: 7 });
-  };
 
   render() {
     return (
@@ -51,25 +12,16 @@ class LifeBar extends React.Component {
           <div
             className="health"
             style={{
-              height: `${this.state.health}%`,
-              backgroundColor: ` ${this.state.lifeBarColor}`,
+              height: `${this.props.health}%`,
+              backgroundColor: ` ${this.props.lifeBarColor}`,
             }}
           ></div>
           <div
             className="bullet"
             style={{
-              backgroundColor: ` ${this.state.lifeBarColor}`,
+              backgroundColor: ` ${this.props.lifeBarColor}`,
             }}
           ></div>
-        </div>
-        <div>
-          <input type="button" onClick={this.getHealth} id="up" value="Up" />
-          <input
-            type="button"
-            onClick={this.getHealth}
-            id="down"
-            value="Down"
-          />
         </div>
       </>
     );
