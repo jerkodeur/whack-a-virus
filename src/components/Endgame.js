fe9ae19690c9f2069a71e7caeef5a5cbf157ebff
orko3012 <or.ko@hotmail.fr>
2020-04-30 20:24:49 +0200

    remove dead code

--- src/components/Endgame.js
+++ src/components/Endgame.js
@@ -2,34 +2,6 @@ import React from "react";
 import "./Endgame.css";
 
 class Endgame extends React.Component {
-  // state = {
-  //     text: '',
-  //     textButton: '',
-  // }
-
-  // defineModal() {
-  //     const modal = document.getElementById('modal')
-  //     if (this.props.verdict === "gagné") {
-  //         modal.style.display = 'flex'
-  //         this.win()
-  //     } else if (this.props.verdict === "perdu") {
-  //         modal.style.display = 'flex'
-  //         this.lose()
-  //     }
-  // }
-
-  // win() {
-
-  //     this.setState({ text: "Yes! Well done, this level is complete !" })
-  //     this.setState({ textButton: "Next Level" })
-  // }
-
-  // lose() {
-
-  //     this.setState({ text: "Too bad, the epidemic won and everybody died !!" })
-  //     this.setState({ textButton: "Try Again" })
-  // }
-
   getModalText() {
     if (this.props.verdict === "gagné") {
       return "Vous avez gagné";
@@ -58,7 +30,6 @@ class Endgame extends React.Component {
             {this.getModalTextButton()}
           </button>
         </div>
-        {/* <div>{this.defineModal()}</div> */}
       </div>
     );
   }
